902be518e31f5fe8a7356eed7cd9657dda47d680
Christine <christinemoore321@gmail.com>
2018-11-06 16:31:16 -0600

    fixed drop down

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,6 +15,12 @@
     }
   });
 
+  $('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
