2d0f98040999a1f570756d95d449edbc2abd20a5
Christine <christinemoore321@gmail.com>
2018-11-06 18:08:36 -0600

    lots of updates

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 // Create the email and send the message
-$to = 'info@ironoakcrossfit.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'christinemoore321@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "New Website Contact Request:  $name";
 $email_body = "Hey Ya'll,\n\n"."Someone tried to contat you through the website. Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@ironoakwebsite.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
